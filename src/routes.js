818583610a4713c72e2459f27f317ea086cb052a
Welington Carlos Alves de Almeida Filho <wcfilho98@hotmail.com>
2023-03-21 09:46:45 -0300

    realizada a remoção de usuários

--- src/routes.js
+++ src/routes.js
@@ -33,6 +33,12 @@ export const routes = [
   {
     method: "DELETE",
     path: buildRoutePath("/users/:id"),
-    handler: (request, response) => {},
+    handler: (request, response) => {
+      const { id } = request.params;
+
+      database.delete("users", id);
+
+      return response.writeHead(204).end();
+    },
   },
 ];
